b4f5d981736efb897c49cf6d0db0245ae3bfafbe
Andrew Yule <andrew.yule@assuredflowsolutions.com>
2022-07-08 15:06:48 -0500

    First working version of shinny app

---
+++ Case_Study_1_Shinny_App/server.R
@@ -0,0 +1,29 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(tidyverse)
+library(shiny)
+
+# Set the theme
+theme_set(theme_bw())
+
+# Load the beers data set
+beers = read_csv("https://www.wolframcloud.com/obj/andrew.yule/DDS_Beers")
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  output$Hist_ABV <- renderPlot({
+    # Draw the histogram with the specified number of bins
+    beers |>
+      ggplot(aes(x = ABV)) +
+      geom_histogram(fill = "#C80F2D", bins = input$uiBins) +
+      labs(x = "ABV (%)", y = "Frequency", title = "Distribution of ABV Values Across Beers") +
+      theme(text=element_text(size=18))
+  })
+})
