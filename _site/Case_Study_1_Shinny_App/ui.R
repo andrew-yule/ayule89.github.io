b9a31e07fee7776d0fa061bc8331e95cefc96b60
Andrew Yule <andrew.yule@assuredflowsolutions.com>
2022-07-10 12:47:37 -0500

    Update: Link posted to shinny app

---
+++ _site/Case_Study_1_Shinny_App/ui.R
@@ -0,0 +1,34 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(tidyverse)
+library(shiny)
+
+# Load the beers data set
+beers = read_csv("https://raw.githubusercontent.com/ayule89/ayule89.github.io/master/Data/beers.csv")
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Beer Data Analysis"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+          radioButtons("uiIBUOrABV", "IBU or ABV:", choices = c("IBU", "ABV"), inline = TRUE),
+          radioButtons("uiHistOrBox", "Histogram or BoxPlot:", choices = c("Histogram", "BoxPlot"), inline = TRUE),
+          sliderInput("uiBins", "Number of bins:", min = 1, max = 50, value = 30),
+          selectInput("uiState", "Select state:", choices = add_row(arrange(unique(select(beers, State)), State), State = "All", .before = 1))
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(plotOutput("plot"))
+    )
+))
